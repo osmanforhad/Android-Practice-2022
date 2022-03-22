e548e71525c5cf6d48bcb38173a369b8c1dded87
Osman Forhad <osmanforhad93@gmail.com>
2022-03-22 13:41:02 +0600

    hide title and titlebar programatically

--- app/src/main/java/com/osmanforhad/myapplication/MainActivity.java
+++ app/src/main/java/com/osmanforhad/myapplication/MainActivity.java
@@ -3,12 +3,17 @@ package com.osmanforhad.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //__Hide Title__//
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //__Hide TitleBar__//
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
